009cb2ece788d5761938f6f7026039df1c3ea1e7
kushalsharma <kushal.sharma@flipkart.com>
2017-02-04 15:44:43 +0530

    Updated class name

--- VirtualTourist/MapController.swift
+++ VirtualTourist/MapController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class MapController: UIViewController, MKMapViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
